fb12e662a28f9ea0b69f9e5d52dd4e04e6540dc2
Yavuz Gedik <yavuzgedik@outlook.com>
2016-10-15 18:20:03 +0300

    rss reader added

--- InfoPan.UW/InfoPan.UW/MainPage.xaml.cs
+++ InfoPan.UW/InfoPan.UW/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Syndication;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +27,38 @@ namespace InfoPan.UW
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        List<SyndicationItem> izsuList = new List<SyndicationItem>(); 
+        private async void Layout_Loaded(object sender, RoutedEventArgs e)
+        {
+            SyndicationClient client = new SyndicationClient();
+            SyndicationFeed feed = await client.RetrieveFeedAsync(new Uri("http://www.izsu.gov.tr/pages/rss.aspx?rssId=2"));
+            if (feed != null)
+            {
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    izsuList.Add(item);
+                }
+            }
+        }
+
+        private async void feedBtn_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in izsuList)
+            {
+                izsuListBox.Items.Add(item.Title.Text);
+            }
+
+            int counter = 0;
+            foreach (var item in izsuList)
+            {
+                izsuListBox.SelectedItem = izsuListBox.Items[counter];
+                izsuListBox.ScrollIntoView(item.Title.Text);
+                webView.NavigateToString(item.Summary.Text);
+                await Task.Delay(1000);
+                counter++;
+            }
         }
     }
 }
